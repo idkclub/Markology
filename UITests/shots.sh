93df1c054cc8df6fe0adf29bd150adfd2b97beb1
Jon <char7f@gmail.com>
2021-02-27 22:03:45 -0500

    Also ipad shots

--- UITests/shots.sh
+++ UITests/shots.sh
@@ -2,11 +2,15 @@
 # brew install chargepoint/xcparse/xcparse
 xcrun simctl boot "iPhone 8 Plus"
 xcrun simctl boot "iPhone 11 Pro Max"
+xcrun simctl boot "iPad Pro (12.9-inch) (4th generation)"
 xcrun simctl ui "iPhone 8 Plus" appearance dark
 xcrun simctl ui "iPhone 11 Pro Max" appearance dark
-xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" | grep xcresult` dark
+xcrun simctl ui "iPad Pro (12.9-inch) (4th generation)" appearance dark
+xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" -destination "name=iPad Pro (12.9-inch) (4th generation)" | grep xcresult`
 xcrun simctl ui "iPhone 8 Plus" appearance light
 xcrun simctl ui "iPhone 11 Pro Max" appearance light
-xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" | grep xcresult` light
+xcrun simctl ui "iPad Pro (12.9-inch) (4th generation)" appearance light
+xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" -destination "name=iPad Pro (12.9-inch) (4th generation)" | grep xcresult`
 xcrun simctl shutdown "iPhone 8 Plus"
 xcrun simctl shutdown "iPhone 11 Pro Max"
+xcrun simctl shutdown "iPad Pro (12.9-inch) (4th generation)"
