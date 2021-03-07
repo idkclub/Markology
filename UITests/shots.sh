b9590316d0780cc18a57c652106c27b24ddff0b3
Jon <char7f@gmail.com>
2021-03-07 14:56:45 -0500

    Allow specifying targets

--- UITests/shots.sh
+++ UITests/shots.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 # brew install chargepoint/xcparse/xcparse
-xcrun simctl boot "iPhone 8 Plus"
-xcrun simctl boot "iPhone 11 Pro Max"
-xcrun simctl boot "iPad Pro (12.9-inch) (2nd generation)"
-xcrun simctl boot "iPad Pro (12.9-inch) (4th generation)"
-xcrun simctl ui "iPhone 8 Plus" appearance dark
-xcrun simctl ui "iPhone 11 Pro Max" appearance dark
-xcrun simctl ui "iPad Pro (12.9-inch) (2nd generation)" appearance dark
-xcrun simctl ui "iPad Pro (12.9-inch) (4th generation)" appearance dark
-xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" -destination "name=iPad Pro (12.9-inch) (2nd generation)" -destination "name=iPad Pro (12.9-inch) (4th generation)" | grep xcresult`
-xcrun simctl ui "iPhone 8 Plus" appearance light
-xcrun simctl ui "iPhone 11 Pro Max" appearance light
-xcrun simctl ui "iPad Pro (12.9-inch) (2nd generation)" appearance light
-xcrun simctl ui "iPad Pro (12.9-inch) (4th generation)" appearance light
-xcparse screenshots --model `xcodebuild test -target UITests -scheme Markology -destination "name=iPhone 8 Plus" -destination "name=iPhone 11 Pro Max" -destination "name=iPad Pro (12.9-inch) (2nd generation)" -destination "name=iPad Pro (12.9-inch) (4th generation)" | grep xcresult`
-xcrun simctl shutdown "iPhone 8 Plus"
-xcrun simctl shutdown "iPhone 11 Pro Max"
-xcrun simctl shutdown "iPad Pro (12.9-inch) (2nd generation)"
-xcrun simctl shutdown "iPad Pro (12.9-inch) (4th generation)"
+if [ "$#" -eq 0 ]
+then
+  targets=('iPhone 8 Plus' 'iPhone 11 Pro Max' 'iPad Pro (12.9-inch) (2nd generation)' 'iPad Pro (12.9-inch) (4th generation)')
+else
+  targets=("${@}")
+fi
+dests=''
+for target in "${targets[@]}"
+do
+  dests="$dests -destination 'name=$target'"
+  xcrun simctl boot "$target"
+  xcrun simctl ui "$target" appearance dark
+done
+xcparse screenshots --model `sh -c "xcodebuild test -target UITests -scheme Markology $dests" | grep xcresult`
+for target in "${targets[@]}"
+do
+  xcrun simctl ui "$target" appearance light
+done
+xcparse screenshots --model `sh -c "xcodebuild test -target UITests -scheme Markology $dests" | grep xcresult`
+for target in "${targets[@]}"
+do
+  xcrun simctl shutdown "$target"
+done
