7ee2bb7b0f55761488f8f33b38408a6ef291fdf1
Jon <char7f@gmail.com>
2021-09-20 19:08:09 -0400

    Remove unused menu items

--- Markology/AppDelegate.swift
+++ Markology/AppDelegate.swift
@@ -1,4 +1,11 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {}
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    override func buildMenu(with builder: UIMenuBuilder) {
+        super.buildMenu(with: builder)
+        builder.remove(menu: .format)
+        builder.remove(menu: .services)
+        builder.remove(menu: .toolbar)
+    }
+}
