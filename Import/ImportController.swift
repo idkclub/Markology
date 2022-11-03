a8b940dd86b5a8ce834bced4ffc0a2a8e9ab53c8
Jon <jon@idk.club>
2022-11-03 03:46:03 -0400

    Empty photo import

---
+++ Import/ImportController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class ImportController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
